9a351031147c87142854b23a5d33cd1065ff7c92
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 18:15:24 +0530

    Add project files.

---
+++ SingletonClass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonClass
+{
+    internal class Program
+    {
+        
+       internal sealed class SingletonClass //to protect from inheritance - even in multithreaded enviornment - use lock to prevent
+        {
+            private static SingletonClass obj;
+            private static Object obj1;
+            private SingletonClass()
+            {
+
+            }
+
+            public static SingletonClass GetObj()
+            {
+                lock (obj1)
+                {
+                    if (obj == null)
+                    {
+                        obj = new SingletonClass();
+                    }
+                }
+                
+                return obj;
+            }
+        }
+
+        
+        static void Main(string[] args)
+        {
+            SingletonClass obj = SingletonClass.GetObj();
+            Test(obj);
+
+            void Test(SingletonClass obj1)
+            {
+                Console.WriteLine("We can use singleton class object as parameter to method");
+            }
+        }
+       
+    }
+
+}
